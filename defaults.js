2ae6430b885958527e7bc6b45ac8e1204469228f
eexperiments <46015678+eexperiments@users.noreply.github.com>
2021-11-14 20:56:15 -0800

    Create defaults.js

---
+++ defaults.js
@@ -0,0 +1,15 @@
+var layout = {
+		showlegend: true,
+		legend: {x:0, y:1.05},
+		title: ' Measurement Of Values',
+		images: [{x: 1,y: 1.05,sizex: 0.22,sizey: 0.22,
+		source: "static/img/image.png",
+		xanchor: "right", xref: "paper",
+		yanchor: "bottom",yref: "paper"}],
+		yaxis:{fixedrange: false,   },
+		xaxis:{fixedrange: false,title:{text:"Id"}},
+		margin:{l:15,r:10}
+	    };
+
+var config = {showLink: false,
+			  responsive: true,};
