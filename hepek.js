92853ef123e64878f1c47ee54faeba0520b9d226
eexperiments <46015678+eexperiments@users.noreply.github.com>
2022-02-05 18:12:02 -0800

    Update hepek.js

--- hepek.js
+++ hepek.js
@@ -27,6 +27,7 @@ function onConnect() {
   console.log("Connected to MQTT server!");
   sendTextToDisplay("Ready to record","Connected to:",userdata["username"],expnum);
   console.log(listento);
+  document.getElementById("hepek_is_online").innerHTML =`<span class="w3-badge w3-green">.</span>Hepek Online`;
   client.subscribe(listento); ////// This is the topic !!!
   
   //message = new Paho.MQTT.Message("Connected to topic "+postto);
@@ -67,6 +68,7 @@ function sendTextToDisplay(line1,line2="",line3="",line4="") {
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
     console.log("onConnectionLost:"+responseObject.errorMessage);
+	  document.getElementById("hepek_is_online").innerHTML =`<span class="w3-badge w3-red">.</span>Hepek Offline`;
   }
 }
 
