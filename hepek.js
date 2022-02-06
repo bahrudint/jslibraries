0212de87a20f042aed17c4831551af7bf02282d9
eexperiments <46015678+eexperiments@users.noreply.github.com>
2022-02-05 18:18:03 -0800

    Update hepek.js

--- hepek.js
+++ hepek.js
@@ -1,84 +1,89 @@
-function addData(datacontainer, x, y, chartID="Chart1") {
-    datacontainer.x.push(x);
-    datacontainer.y.push(y);
-    Plotly.redraw(chartID);
+function addData(datacontainer, x, y, chartID = "Chart1") {
+  datacontainer.x.push(x);
+  datacontainer.y.push(y);
+  Plotly.redraw(chartID);
 }
 
-function removeData(datacontainer,chartID="Chart1") {
-    datacontainer.x=[];
-    datacontainer.y=[];
-    Plotly.redraw(chartID);
+function removeData(datacontainer, chartID = "Chart1") {
+  datacontainer.x = [];
+  datacontainer.y = [];
+  Plotly.redraw(chartID);
 }
 
-var listento     = "Hepek_"+userdata["macid"];
-var postto = "RespToHepek_"+userdata["macid"];
+var listento = "Hepek_" + userdata.macid;
+var postto = "RespToHepek_" + userdata.macid;
 
 
 // MQTT handling
-client = new Paho.MQTT.Client("broker.emqx.io", Number(8084),"/mqtt", "clientId");
+client = new Paho.MQTT.Client("broker.emqx.io", Number(8084), "/mqtt", "clientId");
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 client.connect({
-	onSuccess: onConnect, 
-    useSSL: true
+  onSuccess: onConnect,
+  useSSL: true
 });
 
 function onConnect() {
   console.log("Connected to MQTT server!");
-  sendTextToDisplay("Ready to record","Connected to:",userdata["username"],expnum);
+  sendTextToDisplay("Ready to record", "Connected to:", userdata.username, expnum);
   console.log(listento);
-  document.getElementById("hepek_is_online").innerHTML =`<span class="w3-badge w3-green">.</span>Hepek Online`;
+  document.getElementById("hepek_is_online").innerHTML = `<span class="w3-badge w3-green">.</span>Hepek Online`;
   client.subscribe(listento); ////// This is the topic !!!
-  
+
   //message = new Paho.MQTT.Message("Connected to topic "+postto);
- // message.destinationName = postto;
+  // message.destinationName = postto;
   //client.send(message); 
 }
 
-function getEspData(sensor,interval=300,start="1",rapidNum="1") {
-    /**
-     * sensors: analog, laserDistance,rapidAnalog,pressTemp
-     * interval -> in miliseconds
-     * start:1 stop:0
-    **/
-    var order =  `{"sensor":"${sensor}",
+function getEspData(sensor, interval = 300, start = "1", rapidNum = "1") {
+  /**
+   * sensors: analog, laserDistance,rapidAnalog,pressTemp
+   * interval -> in miliseconds
+   * start:1 stop:0
+   **/
+  var order = `{"sensor":"${sensor}",
                  "interval":"${interval}",
                     "start":"${start}",
                  "rapidNum":"${rapidNum}"}`;
-    message = new Paho.MQTT.Message(order);
-    message.destinationName = postto;
-    client.send(message); 
-    if (start=="1") {sendTextToDisplay("Recording in progres...",sensor);}
-    else {sendTextToDisplay("Recording Done!","Awesome");}
-    // console.log(order);
+  message = new Paho.MQTT.Message(order);
+  message.destinationName = postto;
+  client.send(message);
+  if (start == "1") {
+    sendTextToDisplay("Recording in progres...", sensor);
+  } else {
+    sendTextToDisplay("Recording Done!", "Awesome");
+  }
+  // console.log(order);
 }
 
-function sendTextToDisplay(line1,line2="",line3="",line4="") {
-    var order =  `{"sensor":"display",
+function sendTextToDisplay(line1, line2 = "", line3 = "", line4 = "") {
+  var order = `{"sensor":"display",
                     "line1":"${line1}",
                     "line2":"${line2}",
                     "line3":"${line3}",
                     "line4":"${line4}"}`;
-    message = new Paho.MQTT.Message(order);
-    message.destinationName = postto;
-    setTimeout(() => { client.send(message); }, 200);
+  message = new Paho.MQTT.Message(order);
+  message.destinationName = postto;
+  setTimeout(() => {
+    client.send(message);
+  }, 200);
 }
 
 
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-	  document.getElementById("hepek_is_online").innerHTML =`<span class="w3-badge w3-red">.</span>Hepek Offline`;
+    console.log("onConnectionLost:" + responseObject.errorMessage);
+    document.getElementById("hepek_is_online").innerHTML = `<span class="w3-badge w3-red">.</span>Hepek Offline`;
   }
 }
 
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+  console.log("onMessageArrived:" + message.payloadString);
   var obj = JSON.parse(message.payloadString);
-  var SensorList = ["laserDistance","analog","mass","temp","pressTemp"];
-  for (sensorname in SensorList) {
-    if (sensorname in obj):{
-      addData(datacontainer,obj["millis"],obj[sensorname]);
-    }	
+  var SensorList = ["laserDistance", "analog", "mass", "temp", "pressTemp"];
+  for (let sensorname in SensorList) {
+    if (sensorname in obj) {
+      addData(datacontainer, obj["millis"], obj[sensorname]);
+    }
   }
 }
