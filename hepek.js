0d8e5dbdbd4597fa4a9220dbe57e91f9651b3db4
eexperiments <46015678+eexperiments@users.noreply.github.com>
2022-02-05 22:31:13 -0800

    Update hepek.js

--- hepek.js
+++ hepek.js
@@ -10,7 +10,7 @@ function addData(datacontainer, x, y, chartID = "Chart1") {
 function removeData(datacontainer, chartID = "Chart1") {
   datacontainer.x = [];
   datacontainer.y = [];
-  Plotly.newPlot(chartID, [datacontainer], layout)
+  Plotly.deleteTraces(chartID, 0)
 }
 
 var listento = "Hepek_" + userdata.macid;
